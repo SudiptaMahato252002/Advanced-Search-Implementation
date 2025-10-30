c275e9811015ba4bbd30fceded036cc0c5518de1
SudiptaMahato252002 <sudipmaha123@gmail.com>
2025-10-30 16:37:36 +0530

    minor fixes

--- DatabaseOperations/src/main/java/com/DatabaseOperations/repositories/CategoryRepository.java
+++ DatabaseOperations/src/main/java/com/DatabaseOperations/repositories/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.DatabaseOperations.repositories;
 
-import java.util.Locale.Category;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.DatabaseOperations.entities.Category;
+
+@Repository
 public interface CategoryRepository extends JpaRepository<Category,Long> {
     
 }
--- DatabaseOperations/src/main/java/com/DatabaseOperations/repositories/ProductRepository.java
+++ DatabaseOperations/src/main/java/com/DatabaseOperations/repositories/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.DatabaseOperations.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.DatabaseOperations.entities.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
     
 }
